08acd801944c4e876aff264e5cd566c678a3c719
Samuel-Trujillo <samuel.trujillo093@gmail.com>
2021-06-07 13:45:42 -0700

    commit 1

---
+++ PythagoreanTest.java
@@ -0,0 +1,8 @@
+public class PythagoreanTest {
+    public static void main(String[] args) {
+        Pythagorean iD = new Pythagorean();
+        double hypotenuse = iD.calculateHypotenuse(10,20);
+        System.out.println(hypotenuse);
+    }
+}
+    
